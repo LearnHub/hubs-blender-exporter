eb04e54c3c5c692eb8adb202c77560791ba1c741
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2023-07-11 15:57:40 +0100

    Include "networked" dependency on moveable components

--- addons/io_hubs_addon/components/definitions/moveable.py
+++ addons/io_hubs_addon/components/definitions/moveable.py
@@ -1,6 +1,7 @@
 from bpy.props import BoolProperty
 from ..hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
+from .networked import migrate_networked
 
 
 class Moveable(HubsComponent):
@@ -10,6 +11,15 @@ class Moveable(HubsComponent):
         'category': Category.OBJECT,
         'node_type': NodeType.NODE,
         'panel_type': [PanelType.OBJECT],
+        'deps': ['networked'],
         'icon': 'VIEW_PAN',
-        'version': (1, 0, 0)
+        'version': (1, 0, 1)
     }
+
+    def migrate(self, migration_type, panel_type, instance_version, host, migration_report, ob=None):
+        migration_occurred = False
+        if instance_version < (1, 0, 1):
+            migration_occurred = True
+            migrate_networked(host)
+
+        return migration_occurred
